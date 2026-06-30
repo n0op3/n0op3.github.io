8f7335250b9c339b1cdc3da9eeb69e85bae95a73
n0op3 <n0op3@proton.me>
2026-06-30 18:27:45 +0200

    discord dog link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { siGithub, siProtonmail } from "simple-icons";
+import { siDiscord, siGithub, siProtonmail } from "simple-icons";
 import Icon from "../utils/Icons";
 
 import './Footer.css'
@@ -9,6 +9,7 @@ function Footer() {
             <div class="footer__links">
                 <a href="https://github.com/n0op3" target="_blank"><Icon icon={siGithub} /></a>
                 <a href="mailto:n0op3@proton.me" target="_blank"><Icon icon={siProtonmail} /></a>
+                <a href="https://discord.dog/n0op3" target="_blank"><Icon icon={siDiscord} /></a>
             </div>
         </div>
     );
