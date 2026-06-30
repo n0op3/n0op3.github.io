b542df7aa2f1188c45614cea3e57fda7faa4e368
n0op3 <n0op3@proton.me>
2026-06-30 18:53:36 +0200

    generalized center element

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,14 @@
-import './About.css'
-
 function About() {
-    return (<>
-        <div class="body container">
-            <div class="about">
+    return (
+        <div class="container">
+            <div class="centered">
                 <h1>About Me</h1>
             </div>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
             </p>
         </div>
-    </>);
+    );
 }
 
 export default About
