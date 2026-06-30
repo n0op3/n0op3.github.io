9b6b4fe153486866cf0638017d7f4ebb0b7a54ca
n0op3 <n0op3@proton.me>
2026-06-30 13:36:29 +0200

    avatar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,10 @@ function Hero() {
     return (
         <section class="hero">
             <p class="hero__eyebrow">こんいちは！</p>
+            <img class="hero__avatar" src="https://avatars.githubusercontent.com/u/167058949" />
             <h1 class="hero__headline">My name is n0op3</h1>
             <p class="hero__subheadline">
-                I'm a self-taught software developer with almost a decade of experience.
+                I'm a self-taught software developer with nearly a decade of experience.
             </p>
             <div class="hero__actions">
                 <button class="btn btn--primary">My projects</button>
