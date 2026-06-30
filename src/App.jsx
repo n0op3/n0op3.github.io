f865b8f0559237dd115c0f55899a12c05119537a
n0op3 <n0op3@proton.me>
2026-06-30 14:04:39 +0200

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Hero from "./components/Hero";
+import { HashRouter, Route } from "@solidjs/router";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <>
             <header class="container"><Navbar /></header>
-            <main class="container"><Hero /></main>
+            <HashRouter>
+                <Route path="/" component={Home} />
+            </HashRouter>
             <footer class="container">{/* footer goes here */}</footer>
         </>
-    );
+    )
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+
+function Home() {
+    return (
+        <>
+            <main class="container"><Hero /></main>
+        </>
+    );
+}
+
+export default Home;
