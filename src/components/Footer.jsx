196b7a41b3d87be4bdf9973216b2599983179eed
n0op3 <n0op3@proton.me>
2026-06-30 22:36:49 +0200

    license footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,25 @@
 import { siDiscord, siGithub, siProtonmail } from "simple-icons";
 import Icon from "../utils/Icons";
+import { ScrollText } from "lucide-solid";
 
 import './Footer.css'
 
 function Footer() {
     return (
         <div class="footer">
+            <a href="https://mit-license.org" target="_blank">
+                <div class="footer__license">
+                    <ScrollText />
+                    Licensed <span class="gradient--fg">MIT</span> 2026
+                </div>
+            </a>
+
             <div class="footer__links">
                 <a href="https://github.com/n0op3" target="_blank"><Icon icon={siGithub} /></a>
                 <a href="mailto:n0op3@proton.me" target="_blank"><Icon icon={siProtonmail} /></a>
                 <a href="https://discord.dog/n0op3" target="_blank"><Icon icon={siDiscord} /></a>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
