78456c133877b1138a001a8fd9dd0e1d8e4c3e0e
n0op3 <n0op3@proton.me>
2026-06-30 22:20:45 +0200

    project previews

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
 
 function App() {
     return (
         <>
-            <header ><Navbar /></header>
+            < header > <Navbar /></header >
             <main class="container">
                 <HashRouter>
                     <Route path="/" component={Home} />
                     <Route path="/about" component={About} />
+                    <Route path="/projects" component={Projects} />
                     <Route path="/contact" component={Contact} />
                 </HashRouter>
             </main>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <section class="hero">
             <p class="hero__eyebrow">こんいちは！</p>
             <img class="hero__avatar" src="https://avatars.githubusercontent.com/u/167058949" />
-            <h1 class="hero__headline">My name is <a class="gradient--fg">n0op3</a></h1>
+            <h1 class="hero__headline">My name is <span class="gradient--fg">n0op3</span></h1>
             <p class="hero__subheadline">
                 I'm a self-taught backend software developer with nearly a decade of experience.
             </p>
@@ -13,7 +13,7 @@ function Hero() {
                 <a href="/projects" class="btn btn--primary">My projects</a>
                 <a href="/about" class="btn btn--ghost">More about me</a>
             </div>
-        </section>
+        </section >
     );
 }
 
