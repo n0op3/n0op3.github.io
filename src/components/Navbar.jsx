a286592c928fef4a348b04c659319221685de5af
n0op3 <n0op3@proton.me>
2026-07-01 10:47:17 +0200

    feat: theme switcher

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
 import './Navbar.css'
 
+function ThemeDropdown() {
+    function updateTheme(e) {
+        document.documentElement.setAttribute('data-theme', e.target.value);
+    }
+
+    return (
+        <select onChange={updateTheme}>
+            <option value="inferno">Inferno</option>
+            <option value="tokyo">Tokyo</option>
+        </select>
+    );
+}
+
 function Navbar() {
     return (
         <nav class="navbar">
-            <ul class="navbar__links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About Me</a></li>
-                <li><a href="/projects">Projects</a></li>
-            </ul>
+            <div></div>
+            <div class="navbar__links">
+                <a href="/">Home</a>
+                <a href="/about">About Me</a>
+                <a href="/projects">Projects</a>
+            </div>
+            <div class="navbar__theme-dropdown">
+                <ThemeDropdown />
+            </div>
         </nav>
     );
 }
