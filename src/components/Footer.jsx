530bbddfa47c83d5443fb4d820ecbab3bc05784c
n0op3 <n0op3@proton.me>
2026-07-01 09:36:48 +0200

    contact titles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,9 +17,9 @@ function Footer() {
             <div class="footer__links">
                 <span>Contact</span>
                 <div class="gradient--bg" style="display: flex; justify-content: center; align-items: center; gap: var(--space-3); padding: 8px 8px 0px 8px; margin-top: 0">
-                    <a href="https://github.com/n0op3" target="_blank"><Icon icon={siGithub} /></a>
-                    <a href="mailto:n0op3@proton.me" target="_blank"><Icon icon={siProtonmail} /></a>
-                    <a href="https://discord.dog/n0op3" target="_blank"><Icon icon={siDiscord} /></a>
+                    <a href="https://github.com/n0op3" target="_blank" title="GitHub"><Icon icon={siGithub} /></a>
+                    <a href="mailto:n0op3@proton.me" target="_blank" title="ProtonMail"><Icon icon={siProtonmail} /></a>
+                    <a href="https://discord.dog/n0op3" target="_blank" title="Discord"><Icon icon={siDiscord} /></a>
                 </div >
             </div >
         </div >
