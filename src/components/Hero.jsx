08ac33f2bd3b9ca3fa918fb8f2e6fd9a0eeba522
n0op3 <n0op3@proton.me>
2026-07-01 16:56:45 +0200

    feat: loading animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <section class="hero">
             <p class="hero__eyebrow">こんいちは！</p>
-            <img class="hero__avatar" src="https://avatars.githubusercontent.com/u/167058949" />
+            <img class="hero__avatar" src="https://avatars.githubusercontent.com/u/167058949" onLoad={() => setImgLoaded(true)} />
             <h1 class="hero__headline">My name is <span class="gradient--fg">n0op3</span></h1>
             <p class="hero__subheadline">
                 I'm a self-taught backend software developer with nearly a decade of experience.
@@ -13,7 +13,7 @@ function Hero() {
                 <a href="/projects" class="btn btn--primary">My projects</a>
                 <a href="/about" class="btn btn--ghost">More about me</a>
             </div>
-        </section >
+        </section>
     );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 import 'solid-devtools';
+import App from './App';
 
 import './index.css';
-import App from './App';
+import './styles/animations.css'
 
 const root = document.getElementById('root');
 
