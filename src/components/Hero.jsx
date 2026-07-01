bc4a85df0b9d9e8a3400666e83d50737d3f25a01
n0op3 <n0op3@proton.me>
2026-07-01 19:25:42 +0200

    feat: avatar fade-in animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
+import { createSignal } from 'solid-js';
 import './Hero.css'
 
 function Hero() {
+    const [imageLoaded, setImageLoaded] = createSignal(false);
+
     return (
         <section class="hero">
             <p class="hero__eyebrow">こんいちは！</p>
-            <img class="hero__avatar" src="https://avatars.githubusercontent.com/u/167058949" onLoad={() => setImgLoaded(true)} />
+            <img class={`hero__avatar ${imageLoaded() ? 'loaded' : ''}`}
+                src="https://avatars.githubusercontent.com/u/167058949"
+                onLoad={() => setImageLoaded(true)} />
             <h1 class="hero__headline">My name is <span class="gradient--fg">n0op3</span></h1>
             <p class="hero__subheadline">
                 I'm a self-taught backend software developer with nearly a decade of experience.
