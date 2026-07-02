a0fe867e593afe8a053cd398893dd3eb5fc64d96
n0op3 <n0op3@proton.me>
2026-07-02 11:37:46 +0200

    write the about me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,20 +2,57 @@ function About() {
     return (
         <div class="container">
             <h1 class="centered">About Me</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis veritatis nostrum adipisci eveniet quia harum quae veniam neque omnis distinctio, ipsum magnam inventore ad maxime accusamus et placeat corporis ab.
+            <p class="text-body">
+                Hi! I go by <span class="gradient--bg">n0op3</span> online, and I love to share what I build on my GitHub :)
+                <br />
+                Here's a short version of my journey with programming:
+
+                <br />
+                <br />
+
+                I first started coding as a kid when I played Minecraft - I wanted to learn game development,
+                and even though, in hindsight, Java clearly wasn't the best tool for the job, I picked it simply because
+                Minecraft ran on it. But curiosity continued to grow, and I wanted to learn more about how things worked under
+                the hood. So I started to mod the game, then learned about another way to make games - writing custom engines in <span class="gradient--bg">C++</span>.
+
+                <br />
+                <br />
+
+                And so my journey with C++ began. At first, I had trouble grasping what memory's all about
+                (I was still just a teen at the time), but I persisted and eventually became quite fluent at it.
+                I wouldn't call myself an expert even now, but I'm more than capable of creating stuff in C++.
+
+                <br />
+                <br />
+
+                But then, another thing piqued my interest: a new language, promising performance and power of C++,
+                but with ergonomics and safety of modern programming languages. That's right - <span class="gradient--bg">Rust</span>.
+                <br />
+                At first, it was really hard to make anything with the language - it requires to adopt a new way of thinking, one
+                which is not really present anywhere else. But once I finally stopped fighting the borrow checker, we became best friends -
+                and now it's hard to imagine a world without it.
+
+                <br />
+                <br />
+
+                My specialization is backend and infra, but I recently started dabbling some in frontend - my current <i>magnum opus</i> is
+                this very site. I'm sure to work on those skills until I'm capable in this area, too - but my focus stays on working behind
+                the scenes. I'm also interested in cybersecurity - although that remains a side hobby of mine. I recently made a team with
+                a few of my friends, and together we took 16th place out of 35 at a CTF-style hackathon.
+
+                <br />
+                <br />
+
+                On the rare occasion that I'm not working, I like to read books, listen to music, make some music, and learn Japanese.
+                If that sounds like we could get along, feel free to reach out to me using the links in the bottom right!
+
+                <br />
+                <br />
+                <br />
+
+                &emsp;&emsp;Thanks for reading, and have a lovely day~
+                <br />
+                &emsp;&emsp;&emsp; - <span class="gradient--fg">n0op3</span>
             </p>
         </div>
     );
